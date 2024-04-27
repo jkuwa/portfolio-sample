2a88efa96ca153ca33112a2121f376761cac7887
kuwa <02kuwa14@gmail.com>
2024-04-27 13:40:08 +0900

    ハンバーガーメニュー実装

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+$(function() {
+  $(".js-hamburger").on('click', function() {
+    $(this).toggleClass("is-open");
+    $(".js-nav").toggleClass("is-open");
+
+    if ($(this).text() === 'メニューを開く') {
+      $(".js-hamburgerBar").text('メニューを閉じる');
+    } else {
+      $(".js-hamburgerBar").text('メニューを開く');
+    }
+  });
+
+  $(window).on('resize', function() {
+    const bp = 960;
+    const width = $(this).width();
+  
+    if (width < bp && $(".js-hamburger").hasClass("is-open")) {
+      $(".js-hamburger").removeClass("is-open");
+      $(".js-nav").removeClass("is-open");
+      $(".js-hamburgerBar").text('メニューを開く');
+    }
+  });
+});
